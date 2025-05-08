89cc048dc96abbcbaa09c23d814518fbae30ecd1
akrizakri <amandha.kristinas@gmail.com>
2025-05-08 12:00:47 +0200

    Färdig filter-funktion

---
+++ JS/searchUser.js
@@ -0,0 +1,14 @@
+// SÖKFUNKTION
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener("input", function () {
+  const filter = searchInput.value.toLowerCase();
+
+  const filteredUsers = allUsers.filter(
+    (user) =>
+      user.name.toLowerCase().includes(filter) ||
+      user.username.toLowerCase().includes(filter) ||
+      user.email.toLowerCase().includes(filter)
+  );
+
+  displayUsers(filteredUsers);
+});
